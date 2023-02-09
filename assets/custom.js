9e0a01baf82bacc8d3c336883a3db68a43f7946e
Akash <ak.web.creater@gmail.com>
2023-02-09 12:41:48 +0530

    First Commit

---
+++ assets/custom.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//   $(".mega-menu-block.menu--421045c6-0e1c-49a5-afcf-81de049fd2c8 li.dropdown-submenu.grid__item.large--one-fifth.medium--one-fifth:nth-child(4)").click(function(){
+//     $(".thermoskin-hd1").css({"color": "yellow"});
+//   });
+// });
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  $('.collection-filtering-section,.collection-grid').wrapAll('<div class="layout-fix-collection"></div>')
+
+  
+});
