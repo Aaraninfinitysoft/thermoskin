083cf4f4e9ccfe4d130514305287743860e4ae62
Akash <ak.web.creater@gmail.com>
2023-03-30 15:56:19 +0530

    new price with GST Added

--- assets/custom.js
+++ assets/custom.js
@@ -125,6 +125,7 @@ function priceWIthGST(gst, price) {
 
 // update GST
 function updateGSTprice(currentElm, gstprice, closestWrapperSelector, toBeUpdatedSelector,) {
+  console.log(closestWrapperSelector)
   let span = document.createElement('span');
   span.textContent = gstprice;
   if (closestWrapperSelector == null && toBeUpdatedSelector == null) {
